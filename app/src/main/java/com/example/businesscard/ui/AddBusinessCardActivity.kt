8456ac959c10de8bbdfa74fcd750c230ab8449a9
guinther-erich <guinther.erich@gmail.com>
2022-02-11 11:20:28 -0300

    Set button confirm in AddBusinessCardActivity

--- app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -3,11 +3,20 @@ package com.example.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.businesscard.App
+import com.example.businesscard.R
+import com.example.businesscard.data.BusinessCard
 import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.mbConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                name = binding.tilName.editText?.text.toString(),
+                company = binding.tilCompany.editText?.text.toString(),
+                phone = binding.tilPhone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                backgroundColor = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
